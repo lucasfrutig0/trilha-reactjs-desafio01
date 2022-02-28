36162ff1a79870b86dbd5b65ec83642ee6bd7cab
lucasfrutig <lucasfrutig0@gmail.com>
2022-02-28 12:00:19 -0300

    funções todo: add, remove and edit a task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,16 +14,39 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
+  function randomID(): number {
+    return Math.floor(Math.random() * 100)
+  } 
+
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if (!newTaskTitle) return
+
+    const newTask = {
+      id: randomID(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+    
+    setTasks(prevState => [...prevState, newTask])
+    setNewTaskTitle("")
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const taskToEdit = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task)
+
+    setTasks(taskToEdit)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const taskToDelete = tasks.filter(task => task.id !== id)
+    setTasks(taskToDelete)
   }
 
   return (
